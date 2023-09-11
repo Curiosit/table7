7d887eaa5805efe735466759c4b3dbb86b8e0923
Curiosit <mailing.bot@gmail.com>
2023-09-12 00:05:52 +0200

    Update export.py

--- export.py
+++ export.py
@@ -23,13 +23,35 @@ mongoClient.admin.command('ping')
 
 db = mongoClient['material-library']
 db.command('ping')
-collection = db['materials-table7']
+collection = db['epds']
 csvfilepath = 'src/tabel7.csv'
 #csvfile = open('src/tabel7.csv', 'r')
 reader = pd.read_csv(csvfilepath,encoding = 'UTF-8')
 data_json = json.loads(reader.to_json(orient='records'))
 print(data_json[1])
-collection.insert(data_json)
+print(data_json[1]['NAVN'])
+jsondata = []
+i = 0
+for row in data_json:
+    i = i + 1
+    if i < 100:
+        rowjson = {
+            'name':row['NAVN'],
+            'A1A3':row['A1A3'],
+            'C3':row['C3'],
+            'C4':row['C4'],
+            'D':row['D'],
+            'unit':row['Unit'],
+            'mass':row['Mass'],
+            'url':row['Url']
+        }
+        jsondata.append(rowjson)
+        collection.insert(rowjson)
+print(jsondata)
+""" for row in data_json:
+    print(row)
+    collection.insert(row) """
+#collection.insert(data_json)
 
 
 
