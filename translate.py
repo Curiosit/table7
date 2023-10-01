6f0d22b28278fc88b6a49ca5b21fc9a081392583
Curiosit <mailing.bot@gmail.com>
2023-10-01 18:19:09 +0200

    modified table7 english names

--- translate.py
+++ translate.py
@@ -17,13 +17,13 @@ user = os.getenv('MONGODB_USER')
 password = os.getenv('MONGODB_PASSWORD')
 uri = os.getenv('MONGODB_URI')
 
-mongoClient = MongoClient(uri)
+""" mongoClient = MongoClient(uri)
 MongoClient(username=user, password=password)
 mongoClient.admin.command('ping')
 
 db = mongoClient['material-library']
 db.command('ping')
-collection = db['epds']
+collection = db['epds'] """
 csvfilepath = 'src/tabel7.csv'
 #csvfile = open('src/tabel7.csv', 'r')
 reader = pd.read_csv(csvfilepath,encoding = 'UTF-8')
@@ -34,25 +34,18 @@ jsondata = []
 i = 0
 for row in data_json:
     i = i + 1
-    if i < 600:
+    if i < 1000:
         rowjson = {
             'name':row['NAVN'],
-            'nameen':row['NAME'],
-            'type':row['type'],
-            'A1A3':row['A1A3'],
-            'C3':row['C3'],
-            'C4':row['C4'],
-            'D':row['D'],
-            'unit':row['Unit'],
-            'mass':row['Mass'],
-            'url':row['Url']
+            
         }
-        jsondata.append(rowjson)
-        collection.insert(rowjson)
+        print (rowjson)
+        #jsondata.append(rowjson)
+        #collection.insert(rowjson)
 print(jsondata)
-for row in data_json:
+""" for row in data_json:
     print(row)
-    #collection.insert(row) 
+    collection.insert(row) """
 #collection.insert(data_json)
 
 
